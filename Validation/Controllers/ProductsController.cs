4be433c0a4e4927a689e5e4dcb4da9a861bb0fe7
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-08-26 17:20:55 -0500

    Validation lessons

---
+++ Validation/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using ProductWithValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductWithValidation.Controllers {
+    public class ProductsController : Controller {
+        // GET: Products
+        public ActionResult Index() {
+            return View();
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(Product collection) {
+            if (ModelState.IsValid) {
+
+                // do stuff like add to the database, etc.
+                return RedirectToAction("Index");
+            }
+            else {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ValidateName(string name) {
+            return Json(new Regex(@"^\D*$").IsMatch(name));
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection) {
+            try {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection) {
+            try {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
---
+++ Validation/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductWithValidation.Models {
+
+    public class Product {
+
+        [Remote("ValidateName", "Products", ErrorMessage = "Name can't contain numbers", HttpMethod = "POST")]
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
+        public string Name { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be positive")]
+        public decimal Price { get; set; }
+    }
+}
